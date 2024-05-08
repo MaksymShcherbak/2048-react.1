625b035cfd4e9874bc9a193da4c7486e1a440f16
Maksym Shcherbak <dev.14805@simplelogin.co>
2024-05-08 21:40:38 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let [nums, setNums] = useState([
+  const [nums, setNums] = useState([
     [2, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
   ]);
-
-  function getClass(num) {
-    return "num-" + num;
-  }
   const [score, setScore] = useState(0);
 
   function randomNum(nums) {
@@ -20,9 +16,8 @@ function App() {
       .map((x, index) => {
         if (x === 0) return index;
       })
-      .filter((item) => item !== undefined); //делает масив индексов которые = 0
+      .filter((item) => item !== undefined); // делает масив индексов которые = 0
     if (indexes.length == 0) {
-      console.log("Вы проиграли");
       return nums;
     } else {
       const index = indexes[Math.floor(Math.random() * indexes.length)];
@@ -33,210 +28,177 @@ function App() {
       return newNums;
     }
   }
+
+  function goLeft() {
+    let newNums = JSON.parse(JSON.stringify(nums)); // сделать копию масива nums
+    let newScore = score;
+    for (let block = 0; block <= 2; block++) {
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (newNums[i][j] == 0) {
+            newNums[i][j] = newNums[i][j + 1];
+            newNums[i][j + 1] = 0;
+          }
+          if (newNums[i][j] == newNums[i][j + 1]) {
+            newNums[i][j] = newNums[i][j + 1] + newNums[i][j];
+            newScore += newNums[i][j];
+            newNums[i][j + 1] = 0;
+          }
+        }
+      }
+    }
+    setScore(newScore);
+
+    newNums = randomNum(newNums);
+    setNums(newNums);
+  }
+
+  function goRight() {
+    let newNums = JSON.parse(JSON.stringify(nums)); // сделать копию масива nums
+    let newScore = score;
+    for (let block = 0; block <= 2; block++) {
+      for (let i = 0; i < 4; i++) {
+        for (let j = 3; j > 0; j--) {
+          if (newNums[i][j] == 0) {
+            newNums[i][j] = newNums[i][j - 1];
+            newNums[i][j - 1] = 0;
+          }
+          if (newNums[i][j] == newNums[i][j - 1]) {
+            newNums[i][j] = newNums[i][j - 1] + newNums[i][j];
+            newScore += newNums[i][j];
+            newNums[i][j - 1] = 0;
+          }
+        }
+      }
+    }
+    setScore(newScore);
+
+    newNums = randomNum(newNums);
+    setNums(newNums);
+  }
+
+  function goUp() {
+    let newNums = JSON.parse(JSON.stringify(nums)); // сделать копию масива nums
+
+    let newScore = score;
+    for (let block = 0; block <= 2; block++) {
+      for (let j = 3; j >= 0; j--) {
+        for (let i = 0; i <= 2; i++) {
+          if (newNums[i][j] == 0) {
+            newNums[i][j] = newNums[i + 1][j];
+            newNums[i + 1][j] = 0;
+          }
+          if (newNums[i][j] == newNums[i + 1][j]) {
+            newNums[i][j] = newNums[i + 1][j] + newNums[i][j];
+            newScore += newNums[i][j];
+            newNums[i + 1][j] = 0;
+          }
+        }
+      }
+    }
+    setScore(newScore);
+
+    newNums = randomNum(newNums);
+    setNums(newNums);
+  }
+
+  function goDown() {
+    let newNums = JSON.parse(JSON.stringify(nums)); // сделать копию масива nums
+
+    let newScore = score;
+    for (let block = 0; block <= 2; block++) {
+      for (let j = 3; j >= 0; j--) {
+        for (let i = 3; i >= 1; i--) {
+          if (newNums[i][j] == 0) {
+            newNums[i][j] = newNums[i - 1][j];
+            newNums[i - 1][j] = 0;
+          }
+          if (newNums[i][j] == newNums[i - 1][j]) {
+            newNums[i][j] = newNums[i - 1][j] + newNums[i][j];
+            newScore += newNums[i][j];
+            newNums[i - 1][j] = 0;
+          }
+        }
+      }
+    }
+    setScore(newScore);
+
+    newNums = randomNum(newNums);
+    setNums(newNums);
+  }
+
   function handleKeyDown(event) {
     if (event.key == "w") {
-      console.log(event.key);
-      let newNums = JSON.parse(JSON.stringify(nums)); // зделать копию масива nums
-      console.log(event.key);
-      for (let block = 0; block <= 2; block++) {
-        for (let j = 3; j >= 0; j--) {
-          for (let i = 0; i <= 2; i++) {
-            if (newNums[i][j] == 0) {
-              newNums[i][j] = newNums[i + 1][j];
-              newNums[i + 1][j] = 0;
-            }
-            if (newNums[i][j] == newNums[i + 1][j]) {
-              newNums[i][j] = newNums[i + 1][j] + newNums[i][j];
-              const newScore = score + newNums[i][j];
-              setScore(newScore);
-              newNums[i + 1][j] = 0;
-            }
-          }
-        }
-      }
-
-      newNums = randomNum(newNums);
-      setNums(newNums);
+      goUp();
     }
-
     if (event.key == "s") {
-      console.log(event.key);
-      let newNums = JSON.parse(JSON.stringify(nums)); // зделать копию масива nums
-      console.log(event.key);
-      for (let block = 0; block <= 2; block++) {
-        for (let j = 3; j >= 0; j--) {
-          for (let i = 3; i >= 1; i--) {
-            if (newNums[i][j] == 0) {
-              newNums[i][j] = newNums[i - 1][j];
-              newNums[i - 1][j] = 0;
-            }
-            if (newNums[i][j] == newNums[i - 1][j]) {
-              newNums[i][j] = newNums[i - 1][j] + newNums[i][j];
-              const newScore = score + newNums[i][j];
-              setScore(newScore);
-              newNums[i - 1][j] = 0;
-            }
-          }
-        }
-      }
-
-      newNums = randomNum(newNums);
-      setNums(newNums);
+      goDown();
     }
     if (event.key == "d") {
-      let newNums = JSON.parse(JSON.stringify(nums)); // зделать копию масива nums
-      console.log(event.key);
-      for (let block = 0; block <= 2; block++) {
-        for (let i = 0; i < 4; i++) {
-          for (let j = 3; j > 0; j--) {
-            if (newNums[i][j] == 0) {
-              newNums[i][j] = newNums[i][j - 1];
-              newNums[i][j - 1] = 0;
-            }
-            if (newNums[i][j] == newNums[i][j - 1]) {
-              newNums[i][j] = newNums[i][j - 1] + newNums[i][j];
-              const newScore = score + newNums[i][j];
-              setScore(newScore);
-              newNums[i][j - 1] = 0;
-            }
-          }
-        }
-      }
-      newNums = randomNum(newNums);
-      setNums(newNums);
+      goRight();
     }
     if (event.key == "a") {
-      console.log(event.key);
-      let newNums = JSON.parse(JSON.stringify(nums)); // зделать копию масива nums
-      for (let block = 0; block <= 2; block++) {
-        for (let i = 0; i < 4; i++) {
-          for (let j = 0; j < 3; j++) {
-            if (newNums[i][j] == 0) {
-              newNums[i][j] = newNums[i][j + 1];
-              newNums[i][j + 1] = 0;
-            }
-            if (newNums[i][j] == newNums[i][j + 1]) {
-              newNums[i][j] = newNums[i][j + 1] + newNums[i][j];
-              const newScore = score + newNums[i][j];
-              setScore(newScore);
-              newNums[i][j + 1] = 0;
-            }
-          }
-        }
-      }
-      newNums = randomNum(newNums);
-      setNums(newNums);
+      goLeft();
     }
 
     if (event.key == "ArrowUp") {
-      console.log(event.key);
-      let newNums = JSON.parse(JSON.stringify(nums)); // зделать копию масива nums
-      console.log(event.key);
-      for (let block = 0; block <= 2; block++) {
-        for (let j = 3; j >= 0; j--) {
-          for (let i = 0; i <= 2; i++) {
-            if (newNums[i][j] == 0) {
-              newNums[i][j] = newNums[i + 1][j];
-              newNums[i + 1][j] = 0;
-            }
-            if (newNums[i][j] == newNums[i + 1][j]) {
-              newNums[i][j] = newNums[i + 1][j] + newNums[i][j];
-              const newScore = score + newNums[i][j];
-              setScore(newScore);
-              newNums[i + 1][j] = 0;
-            }
-          }
-        }
-      }
-
-      newNums = randomNum(newNums);
-      setNums(newNums);
+      goUp();
     }
     if (event.key == "ArrowDown") {
-      console.log(event.key);
-      let newNums = JSON.parse(JSON.stringify(nums)); // зделать копию масива nums
-      console.log(event.key);
-      for (let block = 0; block <= 2; block++) {
-        for (let j = 3; j >= 0; j--) {
-          for (let i = 3; i >= 1; i--) {
-            if (newNums[i][j] == 0) {
-              newNums[i][j] = newNums[i - 1][j];
-              newNums[i - 1][j] = 0;
-            }
-            if (newNums[i][j] == newNums[i - 1][j]) {
-              newNums[i][j] = newNums[i - 1][j] + newNums[i][j];
-              const newScore = score + newNums[i][j];
-              setScore(newScore);
-              newNums[i - 1][j] = 0;
-            }
-          }
-        }
-      }
-
-      newNums = randomNum(newNums);
-      setNums(newNums);
+      goDown();
     }
     if (event.key == "ArrowRight") {
-      let newNums = JSON.parse(JSON.stringify(nums)); // зделать копию масива nums
-      console.log(event.key);
-      for (let block = 0; block <= 2; block++) {
-        for (let i = 0; i < 4; i++) {
-          for (let j = 3; j > 0; j--) {
-            if (newNums[i][j] == 0) {
-              newNums[i][j] = newNums[i][j - 1];
-              newNums[i][j - 1] = 0;
-            }
-            if (newNums[i][j] == newNums[i][j - 1]) {
-              newNums[i][j] = newNums[i][j - 1] + newNums[i][j];
-              const newScore = score + newNums[i][j];
-              setScore(newScore);
-              newNums[i][j - 1] = 0;
-            }
-          }
-        }
-      }
-      newNums = randomNum(newNums);
-      setNums(newNums);
+      goRight();
     }
     if (event.key == "ArrowLeft") {
-      console.log(event.key);
-      let newNums = JSON.parse(JSON.stringify(nums)); // зделать копию масива nums
-      for (let block = 0; block <= 2; block++) {
-        for (let i = 0; i < 4; i++) {
-          for (let j = 0; j < 3; j++) {
-            if (newNums[i][j] == 0) {
-              newNums[i][j] = newNums[i][j + 1];
-              newNums[i][j + 1] = 0;
-            }
-            if (newNums[i][j] == newNums[i][j + 1]) {
-              newNums[i][j] = newNums[i][j + 1] + newNums[i][j];
-              const newScore = score + newNums[i][j];
-              setScore(newScore);
-              newNums[i][j + 1] = 0;
-            }
-          }
-        }
-      }
-      newNums = randomNum(newNums);
-      setNums(newNums);
+      goLeft();
     }
   }
 
   return (
-    <>
-      <h2>
-        SCORE
-        {score}
-      </h2>
-      <h2>BEST</h2>
-      <div tabIndex="0" onKeyDown={handleKeyDown} id="all">
-        {nums.flat().map((num, index) => (
-          <div className={getClass(num)} key={index}>
-            <p>{num == 0 ? " " : num}</p>
-          </div>
-        ))}
+    <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
+      <div className="panel-title">
+        <h1>2048</h1>
+        <button>Новая игра</button>
       </div>
-    </>
+      <div className="panel-scores">
+        <div>
+          <h2>Очки</h2>
+          <h1>{score}</h1>
+        </div>
+        <div>
+          <h2>Рекорд</h2>
+          <h1>0</h1>
+        </div>
+      </div>
+      <div className="game">
+        <div className="game-bg">
+          <div className="game-bg-tile">&nbsp;</div>
+          <div className="game-bg-tile">&nbsp;</div>
+          <div className="game-bg-tile">&nbsp;</div>
+          <div className="game-bg-tile">&nbsp;</div>
+          <div className="game-bg-tile">&nbsp;</div>
+          <div className="game-bg-tile">&nbsp;</div>
+          <div className="game-bg-tile">&nbsp;</div>
+          <div className="game-bg-tile">&nbsp;</div>
+          <div className="game-bg-tile">&nbsp;</div>
+          <div className="game-bg-tile">&nbsp;</div>
+          <div className="game-bg-tile">&nbsp;</div>
+          <div className="game-bg-tile">&nbsp;</div>
+          <div className="game-bg-tile">&nbsp;</div>
+          <div className="game-bg-tile">&nbsp;</div>
+          <div className="game-bg-tile">&nbsp;</div>
+          <div className="game-bg-tile">&nbsp;</div>
+        </div>
+        <div className="game-all">
+          {nums.flat().map((num, index) => (
+            <div className={"num-" + num} key={index}>
+              {num == 0 ? <p>&nbsp;</p> : <p>{num}</p>}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
